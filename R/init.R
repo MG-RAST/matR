a181f2af4ef95841dbba1822c459d90052bcb3bd
braithwaite <contact.dtb@gmail.com>
2012-06-22 09:01:01 -0500

    restore repo

---
+++ R/init.R
@@ -0,0 +1,73 @@
+
+############################################
+### PACKAGE INITIALIZATION / LOADING
+############################################
+
+.onAttach <- function (libname, pkgname) {
+	mConfig$server (mConfig$servers()$prod)
+	packageStartupMessage ("matR: server: ", mConfig$server())
+# ... would be good to test the network here ...
+	packageStartupMessage ("matR: not checking network access")
+# ... are there other options we should set?
+# ... for instance: print.default, download.file.method ?
+	options (warn = 1, width = 150, timeout = 300, digits = 2)
+	packageStartupMessage (
+		"matR: this package sets global option(s): warn = 1, width = 150, timeout = 300, digits = 2")
+# Configuration information including auth key resides in a closure
+# defined in matR/data.  This avoids an auth key ending up in .Rhistory or .RData
+	packageStartupMessage ("matR: set your auth key with mConfig$setAuth()")
+
+	packageStartupMessage (paste (
+		"\nThis code is under active development.",
+		"Feedback, feature suggestions, and bug reports are welcome.",
+		"A few things to try:   demo(matR)   package?matR   ?AbundanceRetrieval   utils::example(orgMatrix)", 
+		sep = "\n"))
+	needed <- missingPackages ()
+	if (length (needed) > 0)
+		packageStartupMessage (paste (
+			"\nFunctionality will be limited by missing package(s):\n  ", paste (needed, collapse = " "),
+			"\nTo install, try: dependencies()", sep = ""))
+	}
+
+.Last.lib <- function (libpath) { 
+# nothing to see here
+	}
+
+### we want to facilitate use of this package in parts, and also make installation easy.
+### therefore we minimize strict dependencies in the DESCRIPTION file, check for libraries
+### as necessary, and provide a function to install dependencies
+dependencies <- function () {
+	needed <- missingPackages ()
+	if (length (needed) > 0) {
+		message ("Suggested package(s) from CRAN, Bioconductor, and/or R-Forge are missing:")
+		message ("  ", paste (needed, collapse = " "))
+		message ("Press <return> to choose among known repository mirrors (this will set session options),")
+		message ("or, provide repository URLs to search (one per line, blank line to end):")
+		repos <- scan (what = "character", quiet = TRUE)
+		if (length (repos) == 0) {
+			cat ("\n")
+			chooseCRANmirror (graphics = FALSE)
+			cat ("\n")
+			chooseBioCmirror (graphics = FALSE)
+			cat ("\n")
+			setRepositories (graphics = FALSE)
+			message ("Please wait...")
+			install.packages (needed)
+			}
+		else install.packages (needed, repos = repos)
+		needed <- missingPackages ()
+		if (length (needed) == 0) 
+			message ("\nSuccess!  All suggested packages are installed.",
+				"\nQuit and restart R for changes to take effect.")
+		else message ("\nSuggested package(s) could not be installed: \n  ", paste (needed, collapse = " "))
+		}
+	else message ("All suggested packages are installed.\n",
+			"You may want to run update.packages() if you haven\'t lately.")
+	}
+
+missingPackages <- function () { 
+	P <- installed.packages ()
+# how to check my own name?  that would be slicker
+	D <- strsplit (paste (P ["matR", c ("Imports", "Suggests")], collapse = " "), "[^[:alnum:]\\.]+") [[1]]
+	D [!sapply (D, function (x) { x %in% rownames(P) })]
+	}
---
+++ R/readwrite.R
@@ -0,0 +1,100 @@
+
+#################################################
+### "asFile" is a generic function for exporting
+### matR objects, the natural opposite of constructing
+### an object of the same type from a filename or connection.
+###
+### asFile(x, fname, ...) : creates fname containing objects
+### represented according to the current configuration of export parameters.
+###
+### when x is a list, fname should be a vector of the same length,
+### and each item will be written to a separate file
+###
+### To decide how to write an object, asFile consults (in this order) 
+### export parameters that are:
+###		(1) provided as arguments in the call
+###		(2) set in the "exp" slot of the object, if any
+###		(3) set in the global matR option, "exp"
+### 
+### The point is to provide standardization with 
+### customizability for writing possibly complex
+### types as text and binary files.
+###
+### returns name(s) of written file
+###
+### complementary import functionality should be provided by
+### functions accepting a parameter of class "connection"
+###
+### asFile (<file>) to rewrite in different formats?  well, that is cute...
+#################################################
+
+setGeneric ("asFile", 
+	def = function (x, fname, ...)
+		save (x, file = fname))
+
+setMethod ("asFile",
+	"list",
+	function (x, fname, ...)
+		for (j in 1:length(x)) asFile (x [[j]], fname [j]))
+
+setMethod ("asFile",					# for lists of IDs (this may be stupid)
+	"character",
+	function (x, fname, ...)
+		stop ("matR: unimplemented method for class character"))
+
+setMethod ("asFile",
+	"rlist",
+	function (x, fname, ...)
+		stop ("matR: unimplemented method for class rlist"))
+
+setMethod ("asFile",
+	"matrix",
+	function (x, fname, ...) {
+		args <- list (...)
+		expList <- resolveMerge (args, mConfig$exp())
+		if (expList$type != "binary")
+			write.table (x,
+				file = paste (mConfig$path(), fname, sep = ""),
+				append = expList$append,
+				sep = expList$sep,
+				na = expList$na,
+				row.names = expList$row.names,
+				col.names = expList$col.names)
+		else
+			save (x, file = fname)
+		})
+
+setMethod ("asFile",
+	"Matrix",
+	function (x, fname, ...)
+		asFile (as.matrix (x), fname, ...))
+
+setMethod ("asFile", 
+	"mmatrix",
+	function (x, fname, ...)
+		asFile (as.matrix (x), fname, ...))
+
+#setMethod ("asFile", 
+#	"RBIOM",
+#	function (x, fname, ...) { } )
+#setMethod ("asFile", 
+#	"pcaRes",
+#	function (x, fname, ...) { } )
+### writing PCA to file:  need to concatenate two tables into single object.  snippet from original:
+###	if (! is.null (asFile)) {
+###		write.table (P@R2,	file = asFile, sep = "\t", col.names = FALSE, row.names = TRUE, append = FALSE)
+###		write.table (P (my_pcaRes), file = asFile, sep = "\t", col.names = FALSE, row.names = TRUE, append = TRUE)
+#		}
+#setMethod ("asFile", 
+#	"pca",
+#	function (x, fname, ...) { } )
+#setMethod ("asFile", 
+#	"dendrogram",
+#	function (x, fname, ...) { } )	
+#setMethod ("asFile",
+#	"heatmap",
+#	function (x, fname, ...) { } )
+
+# just an idea:
+reconcileTextParametersWithDefaults <- function (...) { }
+reconcileGraphicsParametersWithDefaults <- function (...) { }
