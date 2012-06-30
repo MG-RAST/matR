78cb64a0ce7cd7eb9163a41c5f9e8bce5d174a6b
Daniel Braithwaite <contact.dtb@gmail.com>
2012-06-29 21:40:10 -0500

    bit more added to docs

--- tests/matrix_large_test.R
+++ tests/matrix_large_test.R
@@ -1,24 +1,26 @@
 
+dontrun <- function () {
 #######################################################
 ### test matrix retrieval across all parameters
 ### 
 ### 
 #######################################################
 
-MIDs <- mMetagenomes ()
+# MIDs <- mMetagenomes ()
 
 small <- sample (length (MIDs), 2)
 medium <- sample (length (MIDs), 20)
 large <- sample (length (MIDs), 200)
 xlarge <- sample (length (MIDs), 2000)
 
-#orgMatrix ()
-#orgMatrixEvalue ()
-#orgMatrixLength ()
-#orgMatrixPercentID ()
 
-#funcMatrix ()
-#funcMatrixEvalue ()
-#funcMatrixLength ()
-#funcMatrixPercentID ()
+orgMatrix ()
+orgMatrixEvalue ()
+orgMatrixLength ()
+orgMatrixPercentID ()
 
+funcMatrix ()
+funcMatrixEvalue ()
+funcMatrixLength ()
+funcMatrixPercentID ()
+}
