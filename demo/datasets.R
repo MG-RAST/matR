e7d200faa0ae5064130a51cd1032f1ba72b62f4e
Daniel Braithwaite <contact.dtb@gmail.com>
2013-08-21 13:02:10 -0500

    Matrix package moved to \'Suggests\' and Matrix classes not imported in NAMESPACE, for faster package load.  added \'reqPack\' calls as needed.  prepackaged binary datasets are now of standard types, only --- not \'collections\', which is proscribed.  inst/extdata/package-data.R builds them.  rewrote \'datasets\' demo to build related \'collections\' in active session.  removed \'view.params\'.  still some question about where \'view.parameters\' etc should be defined.

--- demo/datasets.R
+++ demo/datasets.R
@@ -1,22 +1,17 @@
 
 # BUILD THE EXAMPLE DATASETS INCLUDED WITH matR
+# ...mgp757 was a good set but no longer available
 
-Whalebone <- collection (samples.of.project (9))
+views <- list(L1=view.defaults$raw, L2=view.defaults$raw, L3=view.defaults$raw)
+views$L1[["level"]] <- "level1"
+views$L2[["level"]] <- "level2"
+views$L3[["level"]] <- "level3"
 
-Mat <- collection (samples.of.project (12))
-
-Marine <- collection (samples.of.project (18))
-
-Coral <- collection (samples.of.project (16))
-
-# Hospital <- collection (unlist (mGet ("project", "mgp757", enClass = FALSE) $ analyzed))     # no longer available
-
-guts <- c (cow.rumen.1 = "4441679.3", cow.rumen.2 = "4441680.3", cow.rumen.3 = "4441682.3", fish.gut.1 = "4441695.3", fish.gut.2 = "4441696.3", mouse.lean = "4440463.3", mouse.obese = "4440464.3")
-Guts <- collection (guts)
-
-waters <- c ("4440424.3", "4440423.3", "4440439.3", "4440422.3", "4440412.3", "4440414.3", "4440440.3", "4440413.3", "4440411.3", "4443681.3", "4443682.3", "4443684.3", "4443679.3", "4443683.3", "4443680.3", "4441096.3", "4442583.3", "4441095.3", "4443750.3", "4443762.3", "4443749.3", "4443746.3", "4443747.3", "4443745.3")
-names (waters) <- c (paste ("fresh", 1:15, sep = ""), paste ("spring", 1:9, sep = ""))
-Waters <- collection (waters)
-
-# invisible (sapply (list (Whalebone, Mat, Marine, Coral, Guts, Waters), function (x) try (x$raw)))
-# save (Whalebone, Mat, Marine, Coral, guts, Guts, waters, Waters, file = "~/Desktop/ANL/matR/data/collection-examples.rda", compress = "xz")
+Coral <- collection (coral, views)
+Guts <- collection (guts, views)
+Marine <- collection (marine, views)
+Mat <- collection (mat, views)
+Waters <- collection (waters, views)
+Whalebone <- collection (whalebone, views)
+while (sapply (list (Whalebone, Mat, Marine, Coral, Guts, Waters), function (x) try (x$raw))) ...
+save (Coral, Guts, Marine, Mat, Waters, Whalebone, file="matR-example-collections.rda", compress="xz")
